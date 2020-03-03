4b3b55c2eb0a6921ffdfc818469d7c4d5ba1ba69
Tung Vo <vominhtung@tma.com.vn>
2020-03-03 11:11:12 +0700

    check if user is logged

--- src/main/java/com/vomtung/example/controller/IndexController.java
+++ src/main/java/com/vomtung/example/controller/IndexController.java
@@ -1,9 +1,13 @@
 package com.vomtung.example.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
 
 @Controller
 public class IndexController {
@@ -11,6 +15,16 @@ public class IndexController {
     @RequestMapping(value = { "/" })
     public String login(@RequestParam(value = "error", required = false) final String error, final Model model) {
 
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal == null) {
+            return "index";
+        }
+
+        String username = null;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+        }
+        model.addAttribute("username", username);
         return "index";
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,10 +1,18 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
 <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-  Welcome to our store Spring sercurity 4 </br>
-    <a href="<%=request.getContextPath()%>/login">Login</a>
+    <head>
+        <title>Title</title>
+    </head>
+    <body>
+    <p>Welcome to our store Spring sercurity 4 </p>
+    <c:if test="${not empty username}">
+        <p>hello ${username}</p>
+    </c:if>
+    <c:if test="${empty username}">
+        <p>
+            <a href="<%=request.getContextPath()%>/login">Login</a>
+        </p>
+    </c:if>
   </body>
 </html>
